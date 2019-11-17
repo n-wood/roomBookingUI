1cd74bb184d0ed0ab667d014a0baf0db15531b18
Nathan Wood <nafe.wood@gmail.com>
2019-11-17 21:50:57 +0000

    script to move between pages

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
+
+var users = [    
+     "0--Nathan Wood",
+     "1--Dan Gleeballs"
+]
+
+
+
+var allPageIds = ["welcomePage","whoAreYouPage","whenPage",,"confirmBookingPage"]
+
+function hideAllPages()
+{
+    allPageIds.forEach(element => {
+        document.getElementById(element).style.display = "none";
+    });
+}
+
 function showPage(pageId)
 {
+    
+    hideAllPages();
+
+    switch (pageId) {
+        case "welcomePage":
+            document.getElementById(pageId).style.display = "block";
+            break;
+        case "whoAreYouPage":
+            document.getElementById(pageId).style.display = "block";
+            takeFocusForBadgeScanning();
+            break;
+        case "whenPage":
+            document.getElementById(pageId).style.display = "block";
+            break;
+        case "myBookingsPage":
+            document.getElementById(pageId).style.display = "block";
+            break;
+        case "confirmBookingPage":
+            document.getElementById(pageId).style.display = "block";
+            break;
+        case "confirmCancellationPage":
+            document.getElementById(pageId).style.display = "block";
+            break;
 
 
+    }
+
+}
+
+function lookupUserName(userId)
+{
+
+    for (i = 0; i < users.length; i++) {
+        if (users[i].startsWith(userId +"--"))
+        {
+            return users[i].split("--")[1];
+        }
+      }
+    
+    
 }
 
 function takeFocusForBadgeScanning()
 {
-    
+    document.getElementById("userIdField").focus();
+}
+
+function captureUserId(e)
+{
+
+    var field = document.getElementById("userIdField");
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+       
+        document.getElementById("hiddenUserId").value = field.value;
+        document.getElementById("hiddenUserName").value = lookupUserName(field.value);  
+        showPage("whenPage");
+    }
 }
